d6405c461ca720f22274554ef509f78046527090
Shruti <shrutivaishnava96@gmail.com>
2026-07-18 15:45:40 +0530

    updated github action workflow

--- calculator.py
+++ calculator.py
@@ -1,7 +1,15 @@
+def add(a, b):
+    return a + b
 
-def add(a,b):
-    return a+b
+def subtract(a, b):
+    return a - b
 
-def subtract(a,b):
-    return a-b
+def multiply(a, b):
+    return a * b
 
+num1 = int(input("Enter a number: "))
+num2 = int(input("Enter another number: "))
+
+print(add(num1, num2))
+print(subtract(num1, num2))
+print(multiply(num1, num2))
